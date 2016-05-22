e2b75adaf3ed88949b042b8ada2361e4e764ea19
Craig Nishina <craig.nishina@gmail.com>
2016-05-21 23:33:05 -0700

    exercise(3): user service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,44 @@
 import {Component} from "@angular/core";
+import {HTTP_PROVIDERS} from "@angular/http";
+import {User} from "./shared/user/user";
+import {UserService} from "./shared/user/user.service";
 
 @Component({
   selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  providers: [UserService, HTTP_PROVIDERS],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  user: User;
+  isLoggingIn = true;
+
+  constructor(private _userService: UserService) {
+    this.user = new User();
+  }
+
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+    // TODO: Define
+  }
+  signUp() {
+    this._userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately we were unable to create your account.")
+      );
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
